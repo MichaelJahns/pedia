06e27f091a4703d37ea769321900e3065302a010
MichaelJahns <jahnsmic001@gmail.com>
2023-06-05 14:39:51 -0700

    Builds lite framework to get font demos down with less repeated code Programmatically scales weight of cinzel decorative, displays Nabla

--- app/src/main/java/com/example/pedia/view/VariableFontFragment.kt
+++ app/src/main/java/com/example/pedia/view/VariableFontFragment.kt
@@ -1,6 +1,5 @@
 package com.example.pedia.view
 
-import android.graphics.Typeface
 import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -14,20 +13,15 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.pedia.R
 import com.example.pedia.di.VariableFontViewModel
+import com.example.pedia.view.custom.VariableFontTable
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 @RequiresApi(Build.VERSION_CODES.O)
 class VariableFontFragment : Fragment() {
     val viewModel: VariableFontViewModel by viewModel()
-    private lateinit var text400Builder: Typeface.Builder
-    private lateinit var text700Builder: Typeface.Builder
-    private lateinit var text900Builder: Typeface.Builder
-
     //  UI
-    private lateinit var textW400: TextView
-    private lateinit var textW700: TextView
-    private lateinit var textW900: TextView
-    private lateinit var weightValue: TextView
+    private lateinit var cinzelTitle: TextView
+    private lateinit var cinzelDemo: VariableFontTable
     private lateinit var weightPicker: SeekBar
     private lateinit var colorFontButton: Button
 
@@ -39,34 +33,24 @@ class VariableFontFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_variable_font_demo, container, false)
         initView(view)
         initObservers()
-        setListeners()
+        initListeners()
         return view
     }
 
     private fun initView(view: View) {
-        bindView(view)
-        text400Builder = Typeface.Builder(requireActivity().assets, "fonts/cinzel_decorative.ttf")
-        text700Builder = Typeface.Builder(requireActivity().assets, "fonts/cinzel_decorative.ttf")
-        text900Builder = Typeface.Builder(requireActivity().assets, "fonts/cinzel_decorative.ttf")
-    }
-
-    private fun bindView(view: View) {
-        textW400 = view.findViewById(R.id.weight400)
-        textW700 = view.findViewById(R.id.weight700)
-        textW900 = view.findViewById(R.id.weight900)
-        weightValue = view.findViewById(R.id.weightValue)
+        cinzelTitle = view.findViewById(R.id.cinzelTitle)
+        cinzelDemo = view.findViewById(R.id.cinzelDemo)
         weightPicker = view.findViewById(R.id.weightPicker)
         colorFontButton = view.findViewById(R.id.colorFontCourier)
     }
 
     private fun initObservers() {
         viewModel.weight.observe(viewLifecycleOwner) { weight ->
-            setBuilderWeights(weight)
-            weightValue.text = weight.toString()
+            cinzelDemo.updateWeight(weight)
         }
     }
 
-    private fun setListeners() {
+    private fun initListeners() {
         colorFontButton.setOnClickListener {
             findNavController().navigate(R.id.toColorFontFromVariableFont)
         }
@@ -81,21 +65,5 @@ class VariableFontFragment : Fragment() {
             override fun onStopTrackingTouch(seekBar: SeekBar) {
             }
         })
-    }
-
-    private fun setBuilderWeights(weight: Int) {
-        text400Builder.setFontVariationSettings("'wght' ${weight}, 'slnt' 20, 'ital' 1")
-        text400Builder.setWeight(weight)
-        text700Builder.setFontVariationSettings("'wght' ${weight + 300}, 'slnt' 20, 'ital' 1")
-        text700Builder.setWeight(weight + 300)
-        text900Builder.setFontVariationSettings("'wght' ${weight + 500}, 'slnt' 20, 'ital' 1")
-        text900Builder.setWeight(weight + 500)
-        buildStrings()
-    }
-
-    private fun buildStrings() {
-        textW400.typeface = text400Builder.build()
-        textW700.typeface = text700Builder.build()
-        textW900.typeface = text900Builder.build()
     }
 }
